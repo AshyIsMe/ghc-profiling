587f86e95f63f888b3f0a3114a5a97e7c291f027
Aaron Ash <aaron.ash@gmail.com>
2016-07-01 09:23:15 +0000

    Script fixes

--- run-profile.sh
+++ run-profile.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+RESOLVER1="lts-6.5"
+RESOLVER2="nightly-2016-06-30"
+
 # Initial testing - This is terrible and should be rewritten.  Possibly even written in haskell...
 mkdir work
 cd work || exit
-git clone https://github.com/ekmett/lens/
-cd lens || exit
-stack init
+#lens is a bit too complex to start with... # git clone https://github.com/ekmett/lens/
+git clone https://github.com/acid-state/acid-state
+cd acid-state || exit
+
+#Resolver1
+stack init --resolver "$RESOLVER1" && stack setup && stack build
+stack clean
+#time stack build &> "$RESOLVER1-timing.txt"
+{ time stack build ; } 2> "$RESOLVER1-timing.txt"
+stack clean && rm stack.yaml
+
+#Resolver2
+stack init --resolver "$RESOLVER2" && stack setup && stack build
+stack clean
+#time stack build &> "$RESOLVER2-timing.txt"
+{ time stack build ; } 2> "$RESOLVER2-timing.txt"
+
+stack clean && rm stack.yaml
